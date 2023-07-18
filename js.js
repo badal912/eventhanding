5b0e8715aed80817b5364cfacdc7f52f4a61faa5
badal <khuntbadal@gmail.com>
2023-07-18 11:39:53 +0530

    added new project

---
+++ js.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("button");
+   function random(number) {
+    return Math.floor(Math.random() * (number + 1));
+   }
+
+btn.addEventListener("mouseover", () => {
+    const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+    document.body.style.backgroundColor = rndCol
+});
